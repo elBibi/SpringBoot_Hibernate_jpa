3f56e67b637be0c4ab15a4eb7186f9c68ac6589c
elBibi <my.ajt.knight@gmail.com>
2024-06-19 12:30:36 -0400

    SB 2.5.3, java 1.8, JPA and hibernate initial setup servers starts

---
+++ src/main/java/com/example/SpringBoot_HIbernate_jpa/SpringBootHIbernateJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_HIbernate_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHIbernateJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHIbernateJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBoot_HIbernate_jpa/SpringBootHIbernateJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_HIbernate_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHIbernateJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
